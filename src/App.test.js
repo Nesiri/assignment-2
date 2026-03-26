d0775ff9cc613dea7b61e7fec9453013658c2b54
Nesiri <nesruhussein57@gmail.com>
2026-03-26 11:09:31 -0600

    my name is tested and passed

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('render my name on the page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameElement = screen.getByText(/Nesru Abbamilki/i);
+  expect(nameElement).toBeInTheDocument();
 });
